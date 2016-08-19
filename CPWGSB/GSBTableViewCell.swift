2056a959d3ee45238454695e2e4f17653f7566d8
Peter Spitzer <spitzerpeti@gmail.com>
2016-08-19 15:25:21 +0100

    refactored plus-minus action in tableviewcell

--- CPWGSB/GSBTableViewCell.swift
+++ CPWGSB/GSBTableViewCell.swift
@@ -17,29 +17,22 @@ class GSBTableViewCell: UITableViewCell {
     let quantityMin = 0
     let quantityMax = 100
     
-    @IBAction func minusAction(sender: UIButton) {
+    @IBAction func quantityAction(sender: UIButton) {
         let buttonRow = sender.tag
-        let newValue = bands[buttonRow].quantity - 1
-        
-        if newValue >= quantityMin {
-        quantityCounterLabel.text = "\(newValue)"
-        bands[buttonRow].quantity = newValue
-            
-        ViewController().tableView.reloadData()
+        if sender.currentTitle == "+" {
+            let newValue = bands[buttonRow].quantity + 1
+            if newValue <= quantityMax {
+                quantityCounterLabel.text = "\(newValue)"
+                bands[buttonRow].quantity = newValue
+            }
+        } else {
+            let newValue = bands[buttonRow].quantity - 1
+            if newValue >= quantityMin {
+                quantityCounterLabel.text = "\(newValue)"
+                bands[buttonRow].quantity = newValue
+            }
         }
-        
-        
-    }
-    @IBAction func plusAction(sender: UIButton) {
-        let buttonRow = sender.tag
-        let newValue = bands[buttonRow].quantity + 1
-        
-        if newValue <= quantityMax {
-        quantityCounterLabel.text = "\(newValue)"
-        bands[buttonRow].quantity = newValue
-        
         ViewController().tableView.reloadData()
-        }
     }
 
     override func awakeFromNib() {
